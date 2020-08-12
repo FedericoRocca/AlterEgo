3194e5f424a5e5f112a59bbf4c95942620daed4f
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-08-12 01:00:38 -0300

    Primera versión de movimiento de jugador + navmesh

---
+++ Assets/Scripts/Player/MoveToClickPoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToClickPoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+    bool pressed = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            pressed = true;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            pressed = false;
+        }
+
+        if (pressed)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
